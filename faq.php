a1fb87217f5d1a1b6674831ae520b5f5d1d3c572
RadoslavKamencay <161565745+RadoslavKamencay@users.noreply.github.com>
2024-03-31 20:21:12 +0200

    Uprava stranky na .php

---
+++ faq.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/faq.css">
+  </head>
+<body>
+<header>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+        <a href="index.php"><img src="img/apple.png" height="50"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul class="navbar-nav">
+                <a class="nav-link" href="index.php">Domov</a>
+                <a class="nav-link" href="galeria.php">Galéria</a>
+                <a class="nav-link" href="faq.php">FAQ</a>
+                <a class="nav-link" href="kontakt.php">Kontakt</a>
+            </ul>
+        </div>
+    </nav>
+</header>
+
+<div class="banner">
+    <img src="img/banner.jpg" width="">
+</div>
+
+<section class="container text-center">
+    <div class="row">
+      <div class="col-50"> 
+        <h3>Máte otázky?</h3>
+        <h2>Aké sú hlavné vlastnosti nového modelu iPhone?</h2>
+        <p class="odpoved">Nový model iPhone ponúka vylepšený fotoaparát, výkonnejší procesor a dlhšiu výdrž batérie.</p>
+        <br>
+        <h2>Prečo by som mal/a vybrať iPhone pred inými smartfónmi?</h2>
+        <p class="odpoved">iPhone ponúka spoľahlivý operačný systém, vynikajúcu integráciu s ďalšími zariadeniami Apple a kvalitné spracovanie.</p>
+      </div>
+      <div class="col-50 text-right text-center">
+        <h3>Napíšte nám</h3>
+        <form id="contact" action="thankyou.php">
+          <input type="text" placeholder="Vaše meno" id="meno" required><br>
+          <input type="email" placeholder="Váš email" id="email" required><br>
+          <textarea name="" placeholder="Vaša správa" id="sprava"></textarea><br>
+          <input type="checkbox" name="" id="" required>
+          <label for="">Súhlasím so spracovaním osobných údajov.</label><br>
+          <input type="submit" value="odoslať">
+        </form>
+      </div>
+    </div>
+  </section>
+
+  <footer>
+    <div class="container text-center-end">
+        <div class="row">
+            <div class="col-md-4 mt-4">
+                <h4>Kto sme</h4>
+                <p>Sme značka, ktorá sa sústreďuje nielen na výkon a funkčnosť, ale tiež na eleganciu a štýl, čo sa prejavuje v našich ikonických iPhone zariadeniach.</p>
+            </div>
+            <div class="col-md-4 mt-4">
+                <h4>Kontakt</h4>
+                <p><a href="mailto:kontakt@apple.com">kontakt@apple.com</a></p>
+                <p><a href="tel:0904162468"> 0904 162 468</a></p>
+            </div>
+            <div class="col-md-4 mt-4">
+                <h4>Odkazy</h4>
+                <p><a href="index.php">Domov</a></p>
+                <p><a href="galeria.php">Galéria</a></p>
+                <p><a href="faq.php">FAQ</a></p>
+                <p><a href="kontakt.php">Kontakt</a></p>
+            </div>
+        </div>
+        <div class="copyright text-center">
+            <p>© 2023 O2 Slovakia, s.r.o. Všetky práva vyhradené</p>
+        </div>
+    </div>
+</footer>
+
+    
+
+
+    <script src="js/app.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    
+</body>
+</html>
---
+++ kontakt.php
@@ -0,0 +1,113 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+<body>
+
+<header>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a href="index.php"><img src="img/apple.png" height="50"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul class="navbar-nav">
+                <a class="nav-link" href="index.php">Domov</a>
+                <a class="nav-link" href="galeria.php">Galéria</a>
+                <a class="nav-link" href="faq.php">FAQ</a>
+                <a class="nav-link" href="kontakt.php">Kontakt</a>
+            </ul>
+        </div>
+    </nav>
+</header>
+
+<div class="banner">
+    <img src="img/banner.jpg" alt="">
+</div>
+
+<div class="accordion" id="accordionExample">
+    <div class="accordion-item">
+      <h2 class="accordion-header">
+        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+            Aké sú hlavné rozdiely medzi rôznymi modelmi iPhone?
+        </button>
+      </h2>
+      <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+        <div class="accordion-body">
+            Rozdiely medzi modelmi iPhone sa týkajú veľkosti displeja, výkonu procesora, kvality fotoaparátu a niekedy aj špecifických funkcii, ako je napríklad podpora Apple Pencil.
+        </div>
+      </div>
+    </div>
+    <div class="accordion-item">
+      <h2 class="accordion-header">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+            Ako dlho trvá batéria v priemere v novom iPhone?
+        </button>
+      </h2>
+      <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+        <div class="accordion-body">
+            Batéria v nových modeloch iPhone má odlišnú výdrž v závislosti od konkrétneho modelu. V priemere však môže vydržať od 8 do 12 hodín pri aktívnom používaní.
+        </div>
+      </div>
+    </div>
+    <div class="accordion-item">
+      <h2 class="accordion-header">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+            Aké sú výhody a nevýhody používania iPhone?
+        </button>
+      </h2>
+      <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+        <div class="accordion-body">
+            Výhody zahŕňajú vysokú kvalitu spracovania, spoľahlivosť softvéru a integráciu s ekosystémom Apple. Nevýhody môžu zahŕňať vyššiu cenu v porovnaní s inými značkami a obmedzenia voľby aplikácií mimo App Store.
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+
+
+<footer>
+        <div class="container text-center-end">
+            <div class="row">
+                <div class="col-md-4 mt-4">
+                    <h4>Kto sme</h4>
+                    <p>Sme značka, ktorá sa sústreďuje nielen na výkon a funkčnosť, ale tiež na eleganciu a štýl, čo sa prejavuje v našich ikonických iPhone zariadeniach.</p>
+                </div>
+                <div class="col-md-4 mt-4">
+                    <h4>Kontakt</h4>
+                    <p><a href="mailto:kontakt@apple.com">kontakt@apple.com</a></p>
+                    <p><a href="tel:0904162468"> 0904 162 468</a></p>
+                </div>
+                <div class="col-md-4 mt-4">
+                    <h4>Odkazy</h4>
+                    <p><a href="index.php">Domov</a></p>
+                    <p><a href="galeria.php">Galéria</a></p>
+                    <p><a href="faq.php">FAQ</a></p>
+                    <p><a href="kontakt.php">Kontakt</a></p>
+                </div>
+            </div>
+            <div class="copyright text-center">
+                <p>© 2023 O2 Slovakia, s.r.o. Všetky práva vyhradené</p>
+            </div>
+        </div>
+</footer>
+
+    
+
+
+    <script src="js/app.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    
+</body>
+</html>
